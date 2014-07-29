dc9fecc12b839dd863b5d236d9be0036eae3ac35
rgpass <rgpass@gmail.com>
2014-07-28 21:50:50 -0400

    Troubleshoot Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ GaSpa::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.compress = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
